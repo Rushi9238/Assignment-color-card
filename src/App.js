e45daa15ca6010b7114110cf728db9794ab6cd92
Rushikesh Patil <patilrushikesh8683@gmail.com>
2023-02-25 11:34:53 +0530

    Assignment color card using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card color={"#FF8C00"} colorCode={"#FF8C00"} colorName={"Darkorange"}/>
+      <Card color={"#9932CC"} colorCode={"#9932CC"} colorName={"DarkOrchid"}/>
+      <Card color={"#FF1493"} colorCode={"#FF1493"} colorName={"DeepPink"}/>
+      <Card color={"#D2691E"} colorCode={"#D2691E"} colorName={"Chocolate"}/>
+      <Card color={"#FFA07A"} colorCode={"#FFA07A"} colorName={"LightSalmon"}/>
+      <Card color={"#4169E1"} colorCode={"#4169E1"} colorName={"RoyalBlue"}/>
+      <Card color={"#00FF7F"} colorCode={"#00FF7F"} colorName={"SpringGreen"}/>
+      <Card color={"#87CEEB"} colorCode={"#87CEEB"} colorName={"SkyBlue"}/>
+      <Card color={"#00008B"} colorCode={"#00008B"} colorName={"DarkBlue"}/>
+      <Card color={"#5F9EA0"} colorCode={"#5F9EA0"} colorName={"CadetBlue"}/>
+
     </div>
   );
 }
